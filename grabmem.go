915898803270a313c56ce2ea3b93828b9c1b8fe5
Nehemiah I. Dacres <vivacarlie@gmail.com>
2013-06-12 22:31:23 -0500

    made the decision byte.Buffer or io.ReadFrom

--- grabmem.go
+++ grabmem.go
@@ -1,29 +1,42 @@
 // a program to grab memory over time
 
 package main
-import ( "fmt"
+import (
+ "fmt"
  "io"
  //"bytes"
  "flag"
  "time"
  "crypto/rand"
 )
-var  memsize, step int
+var  memsize, step, max int
 var interval, now, then time.Duration
+var myMemory = make([]byte, step)
+//var myMemory  bytes.Buffer
 
 func usage() {
     fmt.Println("a program to suck up memory")
 }
 func init() {
     flag.IntVar(&memsize, "step",0, "step size for memory increases")
+    flag.IntVar(&max, "max",0, "maximum memory to allcate")
     flag.DurationVar(&interval, "interval", 0, "time between memory steps")
     flag.Parse()
 }
-func main () {
-    myMemory := make([]byte, step)
+func fill() {
+    //myMemory.Grow(step)
     bytesWritten, err := io.ReadFull(rand.Reader, myMemory)
-    if bytesWritten != len(myMemory) || err != nil { 
+    if bytesWritten != len(myMemory) || err != nil {
         fmt.Println("error:",err)
         return
     }
+    time.Sleep(interval)
+}
+func main () {
+    for memsize <= max {
+      myMemory = make([]byte, memsize+step)
+      memsize+=step
+      fill()
+    }
+    usage()
 }
