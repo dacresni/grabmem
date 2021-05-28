e456055adac5dcd17b7e87244a02825f2fc8b6a8
Nehemiah I. Dacres <vivacarlie@gmail.com>
2021-05-28 16:31:19 -0500

    added rust toml

--- grabmem.rs
+++ grabmem.rs
@@ -1,21 +1,36 @@
 //we'll figure out cli parsing later but its 'extern crate getopts;
-use std::{thread, time};
-let mut memsizee = 0i64;// this seemed risky back in 2010
-let mut myMemory = [0;0];
+extern crate getopts;
+use getopts::Options;
+use std::{env, time};
 fn usage( ){
     println!("usage: grabmem max rate step");
-    println!("this program allocates memory in INTERVAL second intervals, increasing by SIZE megabytes of memory,\n");
+    println!("this program allocates memory in RATE second intervals, increasing by STEP megabytes of memory, untill memory reaches MAX.\n");
 }
 fn grab(size: i64) {
-    myMemory = [0;size];//will this get optimized away if we never use it?
+    myMemory = size; //will this get optimized away if we never use it?
     memsize += size;
+    println!("holding {} memory", memsize);
 }
 
 
 fn main(){  
+    let args: Vec<String> = env::args().collect();
+
     usage();
-    grab(1);
-    let one_sec = time::Duration::new(1);
-    thread::sleep(one_sec);
+    let max  = &arg[1]
+    let rate = &arg[2]
+    let step = &arg[3].parse::<i64>()
+    let duration = time::Duration::from_secs(rate);
+    let mut memsize = 0i64
+    let mut myMemory = [i64;step]
+
+    loop { 
+        if memsize >= max {
+            println!("hit max memory");
+            break;
+        }
+        grab(step);
+        thread::sleep(duration);
+    }
 }
 
